28e6a2b45f5860e9b4400ad0cd35c8639a4c02b3
Sedrak Khachatryan <sedrak.g.khachatryan@gmail.com>
2021-03-06 17:44:43 +0400

    LifeCycle and EIP

---
+++ lesson-005/webServer.tf
@@ -0,0 +1,70 @@
+#.........................................................
+# My Terraform
+#
+#Build webServer during bootstrap
+#.........................................................
+
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.my_webserver.id
+}
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-005c06c6de69aee84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Sedrak"
+    l_name = "Khachatryan"
+    names  = ["Vasya", "Alex", "Misha", "Dragon"]
+  })
+
+  tags = {
+    Name  = "Web Server Build by Terraform"
+    Owner = "Sedrak"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My First SecurityGroup"
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Web Server SecurityGroup"
+    Owner = "Sedrak"
+  }
+}
