6d69a48c504683b317719a445241325af1a52ea1
Sedrak Khachatryan <sedrak.g.khachatryan@gmail.com>
2021-03-06 17:44:04 +0400

    Dynamic Blocks in SecurityGroup

---
+++ lesson-004/DynamicSecurityGroup.tf
@@ -0,0 +1,43 @@
+#.........................................................
+# My Terraform
+#
+#Build webServer during bootstrap
+#.........................................................
+
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_security_group" "my_webserver" {
+  name = "Dynamic Security Group"
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "8080", "1541", "9092"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.10.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Dynamic Server SecurityGroup"
+    Owner = "Sedrak"
+  }
+}
